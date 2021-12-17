5e23044758f85d448e3bdd682e2156c2e55e6357
Jim Thompson <jimthompson5802@gmail.com>
2021-12-17 23:48:16 +0000

    set version with dev tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='dask-kubeflow',
-  version='0.1.0',
+  version='0.1.0-dev.0',
   author='Jim Thompson',
   author_email='jimthompson5802@gmail.com',
 #    packages=['package_name', 'package_name.test'],
