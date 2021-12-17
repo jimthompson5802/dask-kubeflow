3ac4670bc048be0ae7d1c792e3ed16dd6e23d3a6
Jim Thompson <jimthompson5802@gmail.com>
2021-12-17 23:40:45 +0000

    initial setup as python package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  name='dask-kubeflow',
+  version='0.1.0',
+  author='Jim Thompson',
+  author_email='jimthompson5802@gmail.com',
+#    packages=['package_name', 'package_name.test'],
+#    scripts=['bin/script1','bin/script2'],
+#    url='http://pypi.python.org/pypi/PackageName/',
+#    license='LICENSE.txt',
+  description='Proof-of-concept to enable DASK in Kubeflow',
+#    long_description=open('README.txt').read(),
+#    install_requires=[
+#        "Django >= 1.1.1",
+#        "pytest",
+#    ],
+)
