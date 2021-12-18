c86f994753bdd7d2911db801462a1b1dd7710e3f
Jim Thompson <jimthompson5802@gmail.com>
2021-12-18 00:54:00 +0000

    refactor: clean-up setup

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ setup(
   version='0.1.0-dev.0',
   author='Jim Thompson',
   author_email='jimthompson5802@gmail.com',
-#    packages=['package_name', 'package_name.test'],
-#    scripts=['bin/script1','bin/script2'],
-#    url='http://pypi.python.org/pypi/PackageName/',
-#    license='LICENSE.txt',
-  description='Proof-of-concept to enable DASK in Kubeflow',
-#    long_description=open('README.txt').read(),
-#    install_requires=[
-#        "Django >= 1.1.1",
-#        "pytest",
-#    ],
+  license='LICENSE',
+  description='Proof-of-concept to enable DASK in Kubeflow'
 )
