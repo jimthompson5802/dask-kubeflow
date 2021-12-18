3b92616091935dbee41bb7049ede5aa27d8685f5
Jim Thompson <jimthompson5802@gmail.com>
2021-12-18 02:22:50 +0000

    corrected constructor method signature

--- dask_kubeflow/core.py
+++ dask_kubeflow/core.py
@@ -7,5 +7,5 @@ logger.setLevel(logging.INFO)
 # TODO: assess integrating into dask-kubernetes
 class KubeflowCluster:
     # TODO: revise constructor parameter list
-    def __init__():
+    def __init__(self):
         pass
