a69f4058f6678e0e758080bedfcff0546c73488a
Jim Thompson <jimthompson5802@gmail.com>
2021-12-18 18:48:29 +0000

    refactor: package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-  name='dask-kubeflow',
+  name='dask_kubeflow',
   version='0.1.0-dev.0',
   author='Jim Thompson',
   author_email='jimthompson5802@gmail.com',
