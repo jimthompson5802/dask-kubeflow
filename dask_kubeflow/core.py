37124a63e11b00d464ba7da99756c6eaee6a0e59
Jim Thompson <jimthompson5802@gmail.com>
2021-12-18 02:21:29 +0000

    checkpoint code

---
+++ dask_kubeflow/core.py
@@ -0,0 +1,11 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# TODO: assess integrating into dask-kubernetes
+class KubeflowCluster:
+    # TODO: revise constructor parameter list
+    def __init__():
+        pass
