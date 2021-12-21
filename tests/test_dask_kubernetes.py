36b20800d2a869602c94e0994f1eb7243b741693
Jim Thompson <jimthompson5802@gmail.com>
2021-12-21 02:17:17 +0000

    checkpoint code

--- tests/test_dask_kubernetes.py
+++ tests/test_dask_kubernetes.py
@@ -3,7 +3,7 @@ import os
 
 print(f'working dir: {os.getcwd()}')
 
-cluster = KubeCluster('worker-spec.yaml', enable_kubeflow=True)
+cluster = KubeCluster('worker-spec.yaml', scheduler_service_wait_timeout=120, enable_kubeflow=True)
 cluster.scale(2)  # specify number of workers explicitly
 
 # cluster.adapt(minimum=1, maximum=100)  # or dynamically scale based on current workload
