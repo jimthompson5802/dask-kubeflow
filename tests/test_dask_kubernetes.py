431aa9a1a726a0d7d187b57fd91265d46dfb9cc4
Jim Thompson <jimthompson5802@gmail.com>
2021-12-22 03:47:00 +0000

    scale to 3 workers

--- tests/test_dask_kubernetes.py
+++ tests/test_dask_kubernetes.py
@@ -4,7 +4,7 @@ import os
 print(f'working dir: {os.getcwd()}')
 
 cluster = KubeCluster('worker-spec.yaml', scheduler_service_wait_timeout=120, enable_kubeflow=True)
-cluster.scale(2)  # specify number of workers explicitly
+cluster.scale(3)  # specify number of workers explicitly
 
 # cluster.adapt(minimum=1, maximum=100)  # or dynamically scale based on current workload
 cluster.close()
