95bcc1e6416dfd8480691a139d3eb16c160efd66
Jim Thompson <jimthompson5802@gmail.com>
2021-12-22 15:04:09 +0000

    hueristic for coordinating kubecluster start up

--- tests/test_dask_kubernetes.py
+++ tests/test_dask_kubernetes.py
@@ -1,11 +1,13 @@
 from dask_kubernetes import KubeCluster
 import os
+from time import sleep
 
 print(f'working dir: {os.getcwd()}')
 
 cluster = KubeCluster('worker-spec.yaml', scheduler_service_wait_timeout=120, enable_kubeflow=True)
-cluster.scale(3)  # specify number of workers explicitly
-
+#print('after Kubecluster'); sleep(10); print('after first sleep, before scale')
+cluster.scale(3)  # specify number of workers 
+print("waiting after scale"); sleep(10); print('after scale delay')
 # cluster.adapt(minimum=1, maximum=100)  # or dynamically scale based on current workload
-cluster.close()
+# cluster.close()
 print('all done')
